1eed80c15ca591811d974766290631997d0917aa
Omar Beckdash <theB@macbook-air.home>
2019-01-03 14:52:08 -0500

    Initial Commit

---
+++ quote_projectTests/quote_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  quote_projectTests.swift
+//  quote_projectTests
+//
+//  Created by Omar Beckdash on 1/3/19.
+//  Copyright © 2019 Omar Bekdash. All rights reserved.
+//
+
+import XCTest
+@testable import quote_project
+
+class quote_projectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
